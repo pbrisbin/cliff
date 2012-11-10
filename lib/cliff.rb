5eaf4edde6e8b6ef6c422f1a8085f1ee9988f0d1
patrick brisbin <pbrisbin@gmail.com>
2012-11-09 20:13:14 -0500

    passing

--- lib/cliff.rb
+++ lib/cliff.rb
@@ -1,5 +1,33 @@
 require "cliff/version"
+require 'yaml'
 
 module Cliff
-  # Your code goes here...
+  class Base
+
+    def self.option(key, type, default)
+      case type
+      when :bool
+        class_eval <<-EOC
+          def self.#{key}?
+            config['#{key}'] || #{default.inspect}
+          end
+        EOC
+      when :string
+        class_eval <<-EOC
+          def self.#{key}
+            config['#{key}'] || #{default.inspect}
+          end
+        EOC
+      end
+    end
+
+    def self.load(str)
+      @config = @config.merge(YAML::load(str))
+    end
+
+    def self.config
+      @config ||= {}
+    end
+
+  end
 end
